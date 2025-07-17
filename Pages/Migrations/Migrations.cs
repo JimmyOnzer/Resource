51fb0021daa411fbde4ae2c98154681008b36b45
Olwethu Matiwane <31886390+JimmyOnzer@users.noreply.github.com>
2025-07-17 19:16:23 +0200

    Add files via upload

---
+++ Pages/Migrations/Migrations.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+namespace WebClient.Pages.Migrations 
+{
+    public partial class InitialCreate : Migration
+    {
+    protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Bookings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                    .Annotation("SqlServer:Identity", "1, 1"),
+                    ResourceId = table.Column<int>(type: "int", nullable: false),
+                    StartTime = table.Column<DateTime>(type: "datetime: true"),
+                    EndTime = table.Column<DateTime>(type: "datetime: true"),
+                    BookedBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    purpose = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bookings", x => x.Id);
+                });
+        }
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+{
+    migrationBuilder.DropTable(
+        name: "Bookings");
+}
+
+}
+}
